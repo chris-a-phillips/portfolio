b0695f98840711f891db8be76c59a25aa275a7b1
atlfinesse <finesse.cap@gmail.com>
2021-03-30 22:33:22 -0700

    Ready for revamp

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -42,8 +42,7 @@ const Navbar = ({ toggle }) => {
 				<Nav scrollNav={scrollNav}>
 					<NavbarContainer>
 						<NavLogo to='/' onClick={toggleHome}>
-						<Favicon src={favicon}/>
-							Chris Phillips
+							CP
 						</NavLogo>
 						<MobileIcon onClick={toggle}>
 							<FaBars />
